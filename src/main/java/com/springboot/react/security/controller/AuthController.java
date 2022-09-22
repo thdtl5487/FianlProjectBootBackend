d09d2d709f113f3d2014dfce7eaf7355d33a10db
thdtl5487 <thdtl5487@gmail.com>
2022-09-22 15:43:08 +0900

    Token 수신 시 유저 정보를 뱉는 기능 개발

--- src/main/java/com/springboot/react/security/controller/AuthController.java
+++ src/main/java/com/springboot/react/security/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.springboot.react.security.controller;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.react.security.dto.MemberRequestDto;
 import com.springboot.react.security.dto.MemberResponseDto;
 import com.springboot.react.security.dto.TokenDto;
+import com.springboot.react.security.jwt.TokenProvider;
+import com.springboot.react.security.model.Member;
+import com.springboot.react.security.repository.MemberRepository;
 import com.springboot.react.security.service.AuthService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +23,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
-
+    
+    private final TokenProvider TP;
+    
+    private final MemberRepository MR;
+        
     @PostMapping("/signup")
     public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto requestDto) {
         return ResponseEntity.ok(authService.signup(requestDto));
@@ -26,6 +35,26 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody MemberRequestDto requestDto) {
+    	System.out.println(requestDto.getUserid() + "@@@@@@@@@@@@@@@@@");
+    	System.out.println(requestDto.getPassword() + "#################");
         return ResponseEntity.ok(authService.login(requestDto));
+    } 
+    
+    @PostMapping("/loginsuccess")
+    public ResponseEntity<Optional<Member>> loginsuccess(@RequestBody TokenDto userToken){
+    	 
+    	System.out.println("받아온값 확인 : " + userToken.getAccessToken());
+    	
+    	System.out.println("유저정보 나옴? : " + TP.getAuthentication(userToken.getAccessToken()).getName());
+    	
+    	Long memId = Long.parseLong(TP.getAuthentication(userToken.getAccessToken()).getName());
+    	
+    	Optional<Member> result = MR.findById(memId);
+    	
+    	System.out.println(result);
+    	
+    	
+    	
+    	return ResponseEntity.ok(result);
     }
 }
